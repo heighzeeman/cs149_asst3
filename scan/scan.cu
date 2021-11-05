6ca609052cd67296cb98c32b57719628237b649b
Matty Wong <wonghzm@stanford.edu>
2021-11-05 12:04:30 -0700

    Preliminary scan - add printfs to debug

--- scan/scan.cu
+++ scan/scan.cu
@@ -47,14 +47,17 @@ static inline int nextPow2(int n) {
 __global__ void upsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
 	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
-	if (i + two_dplus1 <= N) output[i + two_dplus1 - 1] += output[i + two_d - 1];
+	if (i + two_dplus1 <= N) {
+		printf("Thread id = %d, two_d = %d, output[%d] = output[%d]\n", i / two_dplus1, two_d, i + two_dplus1 - 1, i + two_d - 1);
+		output[i + two_dplus1 - 1] += output[i + two_d - 1];
+	}
 }
 
 __global__ void downsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
 	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
 	if (i + two_dplus1 == N) output[N-1] = 0;
-	//__syncthreads();
+	__syncthreads();
 	
 	if (i + two_dplus1 <= N) {
 		int t = output[i+two_d-1];
@@ -76,15 +79,16 @@ void exclusive_scan(int* input, int N, int* result)
     // scan.
 	
 	for (int two_d = 1; two_d <= N/2; two_d*=2) {
-        int two_dplus1 = 2*two_d;
+		int two_dplus1 = 2 * two_d;
 		dim3 num_blocks(((N / two_dplus1) + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK);
+		printf("Upsweep on two_d = %d: num_blocks = %d, N = %d\n", two_d, num_blocks.x, N);
 		upsweep<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, N, result);
 		cudaDeviceSynchronize();
     }
 
     // downsweep phase
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
-        int two_dplus1 = 2*two_d;
+		int two_dplus1 = 2 * two_d;
 		dim3 num_blocks(((N / two_dplus1) + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK);
 		downsweep<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, N, result);
 		cudaDeviceSynchronize();
