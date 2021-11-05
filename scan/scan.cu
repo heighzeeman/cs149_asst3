87872c9c4b1c90f106f0707e97875e7bb0a1c1c5
Matty Wong <wonghzm@stanford.edu>
2021-11-05 14:20:51 -0700

    Preliminary scan - Better attempt to fix int32 overflow

--- scan/scan.cu
+++ scan/scan.cu
@@ -64,8 +64,9 @@ static inline int nextPow2(int n) {
 // places it in result
 __global__ void upsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
-	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
-	if (i + two_dplus1 <= N) {
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < N / two_dplus1) {
+		int i = id * two_dplus1;
 		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2) {
 			dprintf("Thread id = %d, two_d = %d, output[%d] = output[%d]\n", i / two_dplus1, two_d, i + two_dplus1 - 1, i + two_d - 1);
 		}
@@ -75,8 +76,9 @@ __global__ void upsweep(int two_d, int N, int* output) {
 
 __global__ void downsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
-	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
-	if (i + two_dplus1 <= N && i + two_dplus1 > 0) {
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < N / two_dplus1) {
+		int i = id * two_dplus1;
 		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2) {
 			dprintf("Thread id = %d, two_d = %d, output[%d] = output[%d]\n", i / two_dplus1, two_d, i + two_d - 1, i + two_dplus1 - 1);
 		}
