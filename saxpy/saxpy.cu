661f866807bbf7bc58c0c8f1b1ad29e022028e3f
Matty Wong <wonghzm@stanford.edu>
2021-11-05 01:59:22 -0700

    Revert to usual malloc

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -66,9 +66,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     float* device_y = nullptr;
     float* device_result = nullptr;
     
-    cudaHostAlloc(&device_x, N*sizeof(float), cudaHostAllocDefault); 
-	cudaHostAlloc(&device_y, N*sizeof(float), cudaHostAllocDefault);
-	cudaHostAlloc(&device_result, N*sizeof(float), cudaHostAllocDefault);
+    cudaMalloc(&device_x, N*sizeof(float)); 
+	cudaMalloc(&device_y, N*sizeof(float));
+	cudaMalloc(&device_result, N*sizeof(float));
         
     // start timing after allocation of device memory
     double startTime = CycleTimer::currentSeconds();
@@ -95,9 +95,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     double overallDuration = endTime - startTime;
     printf("Effective BW by CUDA saxpy: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, GBPerSec(totalBytes, overallDuration));
 
-	cudaFreeHost(device_x);
-	cudaFreeHost(device_y);
-	cudaFreeHost(device_result);
+	cudaFree(device_x);
+	cudaFree(device_y);
+	cudaFree(device_result);
 }
 
 void printCudaInfo() {
