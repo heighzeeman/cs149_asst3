c3fe893174f0ad41c9f6eda619971ea218c42d2c
Matty Wong <wonghzm@stanford.edu>
2021-11-05 13:12:18 -0700

    Preliminary scan - fix add filtering for printf

--- scan/scan.cu
+++ scan/scan.cu
@@ -54,8 +54,9 @@ __global__ void upsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
 	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
 	if (i + two_dplus1 <= N) {
-		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2)
+		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2) {
 			dprintf("Thread id = %d, two_d = %d, output[%d] = output[%d]\n", i / two_dplus1, two_d, i + two_dplus1 - 1, i + two_d - 1);
+		}
 		output[i + two_dplus1 - 1] += output[i + two_d - 1];
 	}
 }
@@ -64,8 +65,9 @@ __global__ void downsweep(int two_d, int N, int* output) {
 	int two_dplus1 = two_d * 2;
 	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
 	if (i + two_dplus1 <= N) {
-		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2)
+		if (i + two_dplus1 == N || i + two_dplus1 == 0 || i + two_dplus1 == N/2) {
 			dprintf("Thread id = %d, two_d = %d, output[%d] = output[%d]\n", i / two_dplus1, two_d, i + two_d - 1, i + two_dplus1 - 1);
+		}
 		int t = output[i+two_d-1];
         output[i+two_d-1] = output[i+two_dplus1-1];
         output[i+two_dplus1-1] += t;
