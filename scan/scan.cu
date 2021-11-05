1076c9540aec78b1406ec6d1dd26f27754ca348c
Matty Wong <wonghzm@stanford.edu>
2021-11-05 11:44:47 -0700

    Preliminary scan

--- scan/scan.cu
+++ scan/scan.cu
@@ -27,6 +27,8 @@ static inline int nextPow2(int n) {
     return n;
 }
 
+
+
 // exclusive_scan --
 //
 // Implementation of an exclusive scan on global memory array `input`,
@@ -42,6 +44,25 @@ static inline int nextPow2(int n) {
 // Also, as per the comments in cudaScan(), you can implement an
 // "in-place" scan, since the timing harness makes a copy of input and
 // places it in result
+__global__ void upsweep(int two_d, int N, int* output) {
+	int two_dplus1 = two_d * 2;
+	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
+	if (i + two_dplus1 <= N) output[i + two_dplus1 - 1] += output[i + two_d - 1];
+}
+
+__global__ void downsweep(int two_d, int N, int* output) {
+	int two_dplus1 = two_d * 2;
+	int i = (blockIdx.x * blockDim.x + threadIdx.x) * two_dplus1;
+	if (i + two_dplus1 == N) output[N-1] = 0;
+	//__syncthreads();
+	
+	if (i + two_dplus1 <= N) {
+		int t = output[i+two_d-1];
+        output[i+two_d-1] = output[i+two_dplus1-1];
+        output[i+two_dplus1-1] += t;
+	}
+}
+
 void exclusive_scan(int* input, int N, int* result)
 {
 
@@ -53,8 +74,19 @@ void exclusive_scan(int* input, int N, int* result)
     // on the CPU.  Your implementation will need to make multiple calls
     // to CUDA kernel functions (that you must write) to implement the
     // scan.
+	
+	for (int two_d = 1; two_d <= N/2; two_d*=2) {
+        int two_dplus1 = 2*two_d;
+		dim3 num_blocks(((N / two_dplus1) + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK);
+		upsweep<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, N, result);
+    }
 
-
+    // downsweep phase
+    for (int two_d = N/2; two_d >= 1; two_d /= 2) {
+        int two_dplus1 = 2*two_d;
+		dim3 num_blocks(((N / two_dplus1) + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK);
+		downsweep<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, N, result);
+    }
 }
 
 
