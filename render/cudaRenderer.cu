1c7c1f8cfd017ceaf9ac03aca6a9f1b008425106
Matty Wong <wonghzm@stanford.edu>
2021-11-06 01:38:55 -0700

    Back to conservative for comparisons

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -448,7 +448,7 @@ __global__ void kernelRenderCircles() {
 			unsigned circ_idx = 3 * circle;
 			float3 p = *(float3*)(&cuConstRendererParams.position[circ_idx]);
 			float rad = cuConstRendererParams.radius[circle];
-			circleFlag[index] = circleInBox(p.x, p.y, rad, boxL, boxR, boxT, boxB);
+			circleFlag[index] = circleInBoxConservative(p.x, p.y, rad, boxL, boxR, boxT, boxB);
 			#ifdef _DEBUGGING
 			printf("Block (%d, %d) idx %d : circle_idx(not 3) = %d, rad = %f, p.x = %f, p.y = %f\n", blockIdx.x, blockIdx.y, index, circle, rad, p.x, p.y);
 			if (index == 0) {
